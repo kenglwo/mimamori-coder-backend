ba1faf425280b4fd9b2bc98e6d2d8d71cb36f9a2
Kento Shigyo <kentoglwo@gmail.com>
2020-05-13 21:40:06 +0900

    :zap: add setting for cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'api/students_table_items', to: 'students_table#index'
+
+  get 'api/student_view', to: 'student_view#index'
+  get 'api/student_view/file_list', to: 'student_view#file_list'
+  
 end
