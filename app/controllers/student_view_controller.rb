39f24c293addd599c2d6e7564d0a0a0106deccb3
Kento Shigyo <kentoglwo@gmail.com>
2020-05-17 22:22:56 +0900

    :sparkles: update API for student view

--- app/controllers/student_view_controller.rb
+++ app/controllers/student_view_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class StudentViewController < ApplicationController
   def index
     student_id = params['student_id']
-    commit_total_num = `git -C ~/git/#{student_id} log --oneline | wc -l`.strip()
+    commit_total_num = `git -C ~/git/#{student_id} log --oneline | wc -l`.strip
 
     render json: {
       commitTotalNum: commit_total_num,
@@ -24,12 +26,11 @@ class StudentViewController < ApplicationController
   def file_list
     student_id = params['student_id']
     json = {}
-    json["fileNameList"] = []
+    json['fileNameList'] = []
     file_name_list = `git -C ~/git/#{student_id} ls-files`.split("\n")
 
     file_name_list.each do |file_name|
-      json["fileNameList"].push(file_name)
-      
+      json['fileNameList'].push(file_name)
     end
 
     render json: json
@@ -48,29 +49,30 @@ class StudentViewController < ApplicationController
     commit_log = {}
 
     commit_log_info.each_line do |line|
-
-      if line.index("commit") == 0
+      if line.index('commit') == 0
         if commit_log != {}
           json.push(commit_log)
           commit_log = {}
         end
         commit_index += 1
-        commit_log["commitTime"] = commit_time_array[commit_index]
-        commit_log["commitFile"] = []
+        commit_log['commitTime'] = commit_time_array[commit_index]
+        commit_log['commitFile'] = []
       end
-    
-      if line.index("A\t") == 0 or line.index("M\t") == 0 or line.index("D\t") == 0
-        file_info = line.split("\t")
-        commit_file = {
-          fileName: file_info[1].strip(),
-          fileStatus: file_info[0]
-        }
-        commit_log["commitFile"].push(commit_file)
 
-        if commit_index == commit_last_index
-          json.push(commit_log)
-          commit_log = {}
-        end
+      unless (line.index("A\t") == 0) || (line.index("M\t") == 0) || (line.index("D\t") == 0)
+        next
+      end
+
+      file_info = line.split("\t")
+      commit_file = {
+        fileName: file_info[1].strip,
+        fileStatus: file_info[0]
+      }
+      commit_log['commitFile'].push(commit_file)
+
+      if commit_index == commit_last_index
+        json.push(commit_log)
+        commit_log = {}
       end
     end
 
@@ -81,30 +83,66 @@ class StudentViewController < ApplicationController
     student_id = params['student_id']
     current_commit_index = params['current_commit_index']
 
-    commit_total_num = `git -C ~/git/#{student_id} log --oneline | wc -l`.strip()
-    head_hat_num = commit_total_num - current_commit_index
+    commit_total_num = `git -C ~/git/#{student_id} log --oneline | wc -l`.strip
+    head_hat_num = commit_total_num.to_i - current_commit_index.to_i
     head = 'HEAD'
     head_hat_num.times do
       head += '^'
     end
 
-    filenames = `git show --name-only #{head} | sed -n 1,6\!p`.split("\n")
-    commit_time_array = `git -C ~/git/#{student_id} log --oneline --pretty=format:'%cd' --date=format:'%Y/%m/%d %H:%M:%S'`.split("\n")
+    filename_array = `git -C ~/git/#{student_id} show --name-only #{head} | sed -n 1,6\!p`.split("\n")
 
-    json = {}
+    commit_time = `git -C ~/git/#{student_id} show #{head} --pretty=format:'%cd' --date=format:'%Y/%m/%d %H:%M:%S' | head -1`.strip
 
-    # json = [
-    #   {
-    #     fileName: "ok.html",
-    #     commitTime: "2020-05-16 13:28",
-    #     codeString: "",
-    #     codeStatus: "ok"
-    #   }
-    # ]
+    code_string_array = []
+    filename_array.each do |filename|
+      code_string = `git -C ~/git/#{student_id} show #{head}:#{filename}`.strip
+      code_string_array.push(code_string)
+    end
 
-    # render json: json
-    render plain: filenames
+    code_status_array = []
+    code_string_array.each do |_code|
+      # TODO: check the code with linter
+      code_status_array.push('unknown')
+    end
 
+    json = []
+
+    filename_array.each_with_index do |filename, i|
+      code_info = {
+        fileName: filename,
+        commitTime: commit_time,
+        codeString: code_string_array[i],
+        codeStatus: code_status_array[i]
+      }
+      json.push(code_info)
+    end
+
+    render json: json
   end
 
+  def code_string
+    student_id = params['student_id']
+    current_commit_index = params['current_commit_index']
+
+    commit_total_num = `git -C ~/git/#{student_id} log --oneline | wc -l`.strip
+    head_hat_num = commit_total_num.to_i - current_commit_index.to_i
+    head = 'HEAD'
+    head_hat_num.times do
+      head += '^'
+    end
+
+    filename_array = `git -C ~/git/#{student_id} show --name-only #{head} | sed -n 1,6\!p`.split("\n")
+
+    code_string_array = []
+    filename_array.each do |filename|
+      code_string = `git -C ~/git/#{student_id} show #{head}:#{filename}`.strip
+      json = {
+        codeString: code_string
+      }
+      code_string_array.push(json)
+    end
+
+    render json: code_string_array
+  end
 end
