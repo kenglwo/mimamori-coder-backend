c4ba110af8128f2dda88aec2f5af2159941239c4
Kento Shigyo <kentoglwo@gmail.com>
2020-09-08 17:09:10 +0900

    :sparkles: update code to save students' code

--- app/controllers/student_code_controller.rb
+++ app/controllers/student_code_controller.rb
@@ -1,8 +1,26 @@
 class StudentCodeController < ApplicationController
   def save
-    result = ""
+    api_result = ""
+
     code_data = params[:student_code]
-    filename = code_data['studentId']
+    student_id = code_data['student_id']
+    filename = code_data['filename']
     code = code_data['code']
+
+    begin
+      StudentCodeInfo.create(student_code_params)
+      api_result = "Success"
+    rescue => e
+      logger.debug e
+      api_result = e.message
+    end
+
+    render plain: api_result
+
+  end
+
+  private
+  def student_code_params
+    params.require(:student_code).permit(:student_id, :filename, :code)
   end
 end
